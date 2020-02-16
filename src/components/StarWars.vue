cfc92e12e9876da5b7c7376f4186f5fe8b4949d5
aLeX-c-m <alexacatherine.marshall@gmail.com>
2020-02-16 18:00:11 -0500

    adding stylesheet and starwars component

---
+++ src/components/StarWars.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>StarWars:
+  <li >{{wars}}</li>
+  </ul>
+</template>
+
+<script>
+ import axios from 'axios';
+
+  export default {
+    name: 'StarWars',
+    data() {
+      return {
+        wars: null,
+      };
+    },
+    created: function() {
+      axios
+        .get('https://swapi.co/api/people')
+        .then(res => {
+          this.wars = res.data;
+        })
+    }
+  }
+</script>

336ac4375f5bcdb3708e2ca6f14b621324193d75
aLeX-c-m <alexacatherine.marshall@gmail.com>
2020-02-16 22:13:59 -0500

    modularizing list, adding filter buttons

---
+++ src/components/StarWar.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>{{ war.name }}
+  <li v-if="height"> {{ showheight }}</li>
+  <li v-if="mass"> {{ showmass }}</li>
+  </ul>
+</template>
+
+<script>
+
+  export default {
+    name: 'StarWar',
+    props: ['war', 'showheight', 'showmass']
+  }
+</script>
--- src/components/StarWars.vue
+++ src/components/StarWars.vue
@@ -1,25 +1,37 @@
 <template>
+<div>
+<button v-on:click="height = !height">Height</button>
+<button v-on:click="mass = !mass">Weight</button>
   <ul>StarWars:
-  <li >{{wars}}</li>
+  <li v-for="war in wars" v-bind:key="war.url">
+  <StarWar v-bind:war="war" v-bind:showheight= "height" v-bind:showmass="mass" ></StarWar>
+  </li>
   </ul>
+  </div>
 </template>
 
 <script>
  import axios from 'axios';
+ import StarWar from './StarWar.vue';
 
   export default {
     name: 'StarWars',
     data() {
       return {
         wars: null,
+        mass: false,
+        height: false
       };
     },
     created: function() {
       axios
         .get('https://swapi.co/api/people')
         .then(res => {
-          this.wars = res.data;
+          this.wars = res.data.results;
         })
-    }
+    },
+    components: {
+    StarWar
+  }
   }
 </script>
